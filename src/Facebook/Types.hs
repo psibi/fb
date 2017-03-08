38230c5c61139c913d621cae7eb6584460d213dc
Sibi Prabakaran <sibi@psibi.in>
2017-03-08 12:26:10 +0530

    Fix warning

--- src/Facebook/Types.hs
+++ src/Facebook/Types.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE CPP, DeriveDataTypeable, GADTs, StandaloneDeriving #-}
+
 module Facebook.Types
-    ( Credentials(..)
-    , appIdBS
-    , appSecretBS
-    , AccessToken(..)
-    , UserAccessToken
-    , AppAccessToken
-    , AccessTokenData
-    , Id(..)
-    , UserId
-    , accessTokenData
-    , accessTokenExpires
-    , accessTokenUserId
-    , UserKind
-    , AppKind
-    , Argument
-    , (<>)
-    , FbUTCTime(..)
-    ) where
+  ( Credentials(..)
+  , appIdBS
+  , appSecretBS
+  , AccessToken(..)
+  , UserAccessToken
+  , AppAccessToken
+  , AccessTokenData
+  , Id(..)
+  , UserId
+  , accessTokenData
+  , accessTokenExpires
+  , accessTokenUserId
+  , UserKind
+  , AppKind
+  , Argument
+  , (<>)
+  , FbUTCTime(..)
+  ) where
 
 import Control.Applicative ((<$>), (<*>), pure)
 import Control.Monad (mzero)
@@ -28,13 +29,12 @@ import Data.String (IsString)
 import Data.Text (Text)
 import Data.Time (UTCTime, parseTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
-import Data.Typeable (Typeable, Typeable1)
+import Data.Typeable (Typeable)
 #if MIN_VERSION_time(1,5,0)
 import Data.Time (defaultTimeLocale)
 #else
 import System.Locale (defaultTimeLocale)
 #endif
-
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Types as A
 import qualified Data.Text as T
@@ -43,15 +43,13 @@ import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Builder as TLB
 import qualified Data.Text.Lazy.Builder.Int as TLBI
 
-
 -- | Credentials that you get for your app when you register on
 -- Facebook.
-data Credentials =
-    Credentials { appName   :: Text -- ^ Your application name (e.g. for Open Graph calls).
-                , appId     :: Text -- ^ Your application ID.
-                , appSecret :: Text -- ^ Your application secret key.
-                }
-    deriving (Eq, Ord, Show, Read, Typeable)
+data Credentials = Credentials
+  { appName :: Text -- ^ Your application name (e.g. for Open Graph calls).
+  , appId :: Text -- ^ Your application ID.
+  , appSecret :: Text -- ^ Your application secret key.
+  } deriving (Eq, Ord, Show, Read, Typeable)
 
 -- | 'appId' for 'ByteString'.
 appIdBS :: Credentials -> ByteString
@@ -60,7 +58,6 @@ appIdBS = TE.encodeUtf8 . appId
 -- | 'appSecret' for 'ByteString'.
 appSecretBS :: Credentials -> ByteString
 appSecretBS = TE.encodeUtf8 . appSecret
-
 
 -- | An access token.  While you can make some API calls without
 -- an access token, many require an access token and some will
@@ -80,8 +77,9 @@ appSecretBS = TE.encodeUtf8 . appSecret
 -- phantom type on 'AccessToken', which can be 'UserKind' or
 -- 'AppKind'.
 data AccessToken kind where
-    UserAccessToken :: UserId -> AccessTokenData -> UTCTime -> AccessToken UserKind
-    AppAccessToken  :: AccessTokenData -> AccessToken AppKind
+        UserAccessToken ::
+          UserId -> AccessTokenData -> UTCTime -> AccessToken UserKind
+        AppAccessToken :: AccessTokenData -> AccessToken AppKind
 
 -- | Type synonym for @'AccessToken' 'UserKind'@.
 type UserAccessToken = AccessToken UserKind
@@ -89,46 +87,47 @@ type UserAccessToken = AccessToken UserKind
 -- | Type synonym for @'AccessToken' 'AppKind'@.
 type AppAccessToken = AccessToken AppKind
 
+deriving instance Eq (AccessToken kind)
 
-deriving instance Eq   (AccessToken kind)
-deriving instance Ord  (AccessToken kind)
+deriving instance Ord (AccessToken kind)
+
 deriving instance Show (AccessToken kind)
-deriving instance Typeable1 AccessToken
 
+deriving instance Typeable AccessToken
 
 -- | The access token data that is passed to Facebook's API
 -- calls.
 type AccessTokenData = Text
 
 -- | The identification code of an object.
-newtype Id = Id { idCode :: Text }
-    deriving (Eq, Ord, Show, Read, Typeable, IsString)
+newtype Id = Id
+  { idCode :: Text
+  } deriving (Eq, Ord, Show, Read, Typeable, IsString)
 
 instance A.FromJSON Id where
-    parseJSON (A.Object v) = v A..: "id"
-    parseJSON (A.String s) = pure $ Id s
-    parseJSON (A.Number d) = pure $ Id $ from $ floor d
-      where from i = TL.toStrict $ TLB.toLazyText $ TLBI.decimal (i :: Int64)
-    parseJSON o = fail $ "Can't parse Facebook.Id from " ++ show o
+  parseJSON (A.Object v) = v A..: "id"
+  parseJSON (A.String s) = pure $ Id s
+  parseJSON (A.Number d) = pure $ Id $ from $ floor d
+    where
+      from i = TL.toStrict $ TLB.toLazyText $ TLBI.decimal (i :: Int64)
+  parseJSON o = fail $ "Can't parse Facebook.Id from " ++ show o
 
 instance A.ToJSON Id where
-    toJSON (Id t) = A.String t
-
+  toJSON (Id t) = A.String t
 
 -- | A Facebook user ID such as @1008905713901@.
 type UserId = Id
 
-
 -- | Get the access token data.
 accessTokenData :: AccessToken anyKind -> AccessTokenData
 accessTokenData (UserAccessToken _ d _) = d
-accessTokenData (AppAccessToken d)      = d
+accessTokenData (AppAccessToken d) = d
 
 -- | Expire time of an access token.  It may never expire, in
 -- which case it will be @Nothing@.
 accessTokenExpires :: AccessToken anyKind -> Maybe UTCTime
 accessTokenExpires (UserAccessToken _ _ expt) = Just expt
-accessTokenExpires (AppAccessToken _)         = Nothing
+accessTokenExpires (AppAccessToken _) = Nothing
 
 -- | Get the user ID of an user access token.
 accessTokenUserId :: UserAccessToken -> UserId
@@ -136,58 +135,51 @@ accessTokenUserId (UserAccessToken uid _ _) = uid
 
 -- | Phantom type used mark an 'AccessToken' as an user access
 -- token.
-data UserKind deriving (Typeable)
+data UserKind
+  deriving (Typeable)
 
 -- | Phantom type used mark an 'AccessToken' as an app access
 -- token.
-data AppKind deriving (Typeable)
-
+data AppKind
+  deriving (Typeable)
 
 -- | An argument given to an API call.
 type Argument = (ByteString, ByteString)
 
-
 -- | Synonym for 'mappend'.
-(<>) :: Monoid a => a -> a -> a
+(<>)
+  :: Monoid a
+  => a -> a -> a
 (<>) = mappend
 
-
 ----------------------------------------------------------------------
-
-
 -- | /Since 0.14.9./ Not a Facebook JSON format, but a custom @fb@
 -- format for convenience if you need to serialize access tokens.
 instance A.ToJSON (AccessToken kind) where
   toJSON (UserAccessToken uid data_ expires) =
     A.object
-      [ "kind"    A..= ("user" :: Text)
-      , "id"      A..= uid
-      , "token"   A..= data_
-      , "expires" A..= expires ]
+      [ "kind" A..= ("user" :: Text)
+      , "id" A..= uid
+      , "token" A..= data_
+      , "expires" A..= expires
+      ]
   toJSON (AppAccessToken data_) =
-    A.object
-      [ "kind"    A..= ("app" :: Text)
-      , "token"   A..= data_ ]
-
+    A.object ["kind" A..= ("app" :: Text), "token" A..= data_]
 
 -- | (Internal) Since the user of 'parseJSON' is going to choose
 -- via its @kind@ whether a 'UserAccessToken' or an
 -- 'AppAccessToken' is wanted, we need this type class to
 -- implement 'FromJSON'.
-class ParseAccessToken kind where
+class ParseAccessToken kind  where
   parseTokenJSON :: A.Object -> A.Parser (AccessToken kind)
 
 instance ParseAccessToken UserKind where
   parseTokenJSON v =
     checkKind v "user" $
-      UserAccessToken <$> v A..: "id"
-                      <*> v A..: "token"
-                      <*> v A..: "expires"
+    UserAccessToken <$> v A..: "id" <*> v A..: "token" <*> v A..: "expires"
 
 instance ParseAccessToken AppKind where
-  parseTokenJSON v =
-    checkKind v "app" $
-      AppAccessToken <$> v A..: "token"
+  parseTokenJSON v = checkKind v "app" $ AppAccessToken <$> v A..: "token"
 
 -- | (Internal) Used to implement 'parseTokenJSON'.
 checkKind :: A.Object -> Text -> A.Parser a -> A.Parser a
@@ -195,20 +187,19 @@ checkKind v kind ok = do
   kind' <- v A..: "kind"
   if kind == kind'
     then ok
-    else fail $ "Expected access token kind " <> show kind <>
-                " but found " <> show kind' <> "."
+    else fail $
+         "Expected access token kind " <> show kind <> " but found " <> show kind' <>
+         "."
 
 -- | /Since 0.14.9./ Parses the format that 'ToJSON' produces.
 -- Note that you need to statically decide whether you want to
 -- parse a user access token or an app access token.
-instance ParseAccessToken kind => A.FromJSON (AccessToken kind) where
+instance ParseAccessToken kind =>
+         A.FromJSON (AccessToken kind) where
   parseJSON (A.Object v) = parseTokenJSON v
-  parseJSON _            = mzero
-
+  parseJSON _ = mzero
 
 ----------------------------------------------------------------------
-
-
 -- | @newtype@ for 'UTCTime' that follows Facebook's
 -- conventions of JSON parsing.
 --
@@ -217,14 +208,17 @@ instance ParseAccessToken kind => A.FromJSON (AccessToken kind) where
 --
 --  * As a number, 'FbUTCTime' accepts a number of seconds since
 --    the Unix epoch.
-newtype FbUTCTime = FbUTCTime { unFbUTCTime :: UTCTime }
-  deriving (Eq, Ord, Show, Read, Typeable)
+newtype FbUTCTime = FbUTCTime
+  { unFbUTCTime :: UTCTime
+  } deriving (Eq, Ord, Show, Read, Typeable)
 
 instance A.FromJSON FbUTCTime where
   parseJSON (A.String t) =
     case parseTime defaultTimeLocale "%FT%T%z" (T.unpack t) of
       Just d -> return (FbUTCTime d)
-      _      -> fail $ "could not parse FbUTCTime string " ++ show t
+      _ -> fail $ "could not parse FbUTCTime string " ++ show t
   parseJSON (A.Number n) =
     return $ FbUTCTime $ posixSecondsToUTCTime $ fromInteger $ floor n
-  parseJSON _ = fail "could not parse FbUTCTime from something which is not a string or number"
+  parseJSON _ =
+    fail
+      "could not parse FbUTCTime from something which is not a string or number"
