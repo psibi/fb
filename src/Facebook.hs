1d90366ab574b328a07ffa80f476b821989ce0c1
Sibi Prabakaran <sibi@psibi.in>
2017-03-08 11:53:25 +0530

    Export new functions from Facebook module

--- src/Facebook.hs
+++ src/Facebook.hs
@@ -1,137 +1,130 @@
 module Facebook
-    ( -- * @FacebookT@ monad transformer
-      FacebookT
-    , runFacebookT
-    , runNoAuthFacebookT
-    , mapFacebookT
-    , beta_runFacebookT
-    , beta_runNoAuthFacebookT
-    , Auth
-    , NoAuth
+  ( FacebookT
+  , runFacebookT
+  , runNoAuthFacebookT
+  , mapFacebookT
+  , beta_runFacebookT
+  , beta_runNoAuthFacebookT
+  , Auth
+  , NoAuth
+   -- * Authorization and Authentication
+   -- ** Credentials
+  , Credentials(..)
+   -- ** Access token
+  , AccessToken(..)
+  , UserAccessToken
+  , AppAccessToken
+  , AccessTokenData
+  , hasExpired
+  , isValid
+   -- ** App access token
+  , AppKind
+  , getAppAccessToken
+   -- ** User access token
+  , UserKind
+  , RedirectUrl
+  , Permission
+  , getUserAccessTokenStep1
+  , getUserAccessTokenStep2
+  , getUserLogoutUrl
+  , extendUserAccessToken
+  , debugToken
+  , DebugToken(..)
+   -- ** Signed requests
+  , parseSignedRequest
+   -- * Facebook's Graph API
+   -- ** User
+  , User(..)
+  , UserId
+  , Gender(..)
+  , getUser
+  , searchUsers
+  , getUserCheckins
+  , Friend(..)
+  , getUserFriends
+  , getUserFriendLists
+   -- ** Page
+  , Page(..)
+  , getPage
+  , getPage_
+  , searchPages
+   -- ** Actions
+  , Action
+  , createAction
+   -- ** Checkins
+  , Checkin(..)
+  , CheckinFrom(..)
+  , getCheckin
+  , createCheckin
+   -- ** Order
+  , Order(..)
+  , OrderId
+  , OrderApplication
+  , OrderStatus
+  , getOrder
+   -- ** Friend list
+  , FriendList(..)
+  , FriendListType(..)
+  , getFriendListMembers
+   -- * Facebook's Graph API basic functionality
+   -- ** Simple types
+  , ( #= )
+  , SimpleType(..)
+  , FbUTCTime(..)
+   -- ** Complex types
+  , Place(..)
+  , Location(..)
+  , GeoCoordinates(..)
+  , Tag(..)
+   -- ** Pagination
+  , Pager(..)
+  , fetchNextPage
+  , fetchPreviousPage
+  , fetchAllNextPages
+  , fetchAllPreviousPages
+   -- * Real-time update notifications
+   -- ** Subscriptions
+  , modifySubscription
+  , listSubscriptions
+  , RealTimeUpdateObject(..)
+  , RealTimeUpdateField
+  , RealTimeUpdateUrl
+  , RealTimeUpdateToken
+  , RealTimeUpdateSubscription(..)
+   -- ** Notifications
+  , verifyRealTimeUpdateNotifications
+  , getRealTimeUpdateNotifications
+  , RealTimeUpdateNotification(..)
+  , RealTimeUpdateNotificationUserEntry(..)
+   -- * FQL
+  , fqlQuery
+  , FQLTime(..)
+  , FQLList(..)
+  , FQLObject(..)
+   -- * Test User API
+  , getTestUsers
+  , disassociateTestuser
+  , removeTestUser
+  , createTestUser
+  , makeFriendConn
+  , incompleteTestUserAccessToken
+  , TestUser(..)
+  , CreateTestUser(..)
+  , CreateTestUserInstalled(..)
+   -- * Raw access to the Graph API
+  , getObject
+  , postObject
+  , deleteObject
+  , searchObjects
+  , Id(..)
+  , Argument
+   -- * Exceptions
+  , FacebookException(..)
+   -- * Internal functions
+  , unPermission
+  ) where
 
-      -- * Authorization and Authentication
-      -- ** Credentials
-    , Credentials(..)
-      -- ** Access token
-    , AccessToken(..)
-    , UserAccessToken
-    , AppAccessToken
-    , AccessTokenData
-    , hasExpired
-    , isValid
-      -- ** App access token
-    , AppKind
-    , getAppAccessToken
-      -- ** User access token
-    , UserKind
-    , RedirectUrl
-    , Permission
-    , getUserAccessTokenStep1
-    , getUserAccessTokenStep2
-    , getUserLogoutUrl
-    , extendUserAccessToken
-    , debugToken
-    , DebugToken(..)
-      -- ** Signed requests
-    , parseSignedRequest
-
-      -- * Facebook's Graph API
-      -- ** User
-    , User(..)
-    , UserId
-    , Gender(..)
-    , getUser
-    , searchUsers
-    , getUserCheckins
-    , Friend(..)
-    , getUserFriends
-    , getUserFriendLists
-      -- ** Page
-    , Page(..)
-    , getPage
-    , searchPages
-      -- ** Actions
-    , Action
-    , createAction
-      -- ** Checkins
-    , Checkin(..)
-    , CheckinFrom(..)
-    , getCheckin
-    , createCheckin
-      -- ** Order
-    , Order(..)
-    , OrderId
-    , OrderApplication
-    , OrderStatus
-    , getOrder
-      -- ** Friend list
-    , FriendList(..)
-    , FriendListType(..)
-    , getFriendListMembers
-
-      -- * Facebook's Graph API basic functionality
-      -- ** Simple types
-    , (#=)
-    , SimpleType(..)
-    , FbUTCTime(..)
-      -- ** Complex types
-    , Place(..)
-    , Location(..)
-    , GeoCoordinates(..)
-    , Tag(..)
-      -- ** Pagination
-    , Pager(..)
-    , fetchNextPage
-    , fetchPreviousPage
-    , fetchAllNextPages
-    , fetchAllPreviousPages
-
-      -- * Real-time update notifications
-      -- ** Subscriptions
-    , modifySubscription
-    , listSubscriptions
-    , RealTimeUpdateObject(..)
-    , RealTimeUpdateField
-    , RealTimeUpdateUrl
-    , RealTimeUpdateToken
-    , RealTimeUpdateSubscription(..)
-      -- ** Notifications
-    , verifyRealTimeUpdateNotifications
-    , getRealTimeUpdateNotifications
-    , RealTimeUpdateNotification(..)
-    , RealTimeUpdateNotificationUserEntry(..)
-
-      -- * FQL
-    , fqlQuery
-    , FQLTime(..)
-    , FQLList(..)
-    , FQLObject(..)
-
-      -- * Test User API
-    , getTestUsers
-    , removeTestUser
-    , createTestUser
-    , makeFriendConn
-    , incompleteTestUserAccessToken
-    , TestUser(..)
-    , CreateTestUser(..)
-    , CreateTestUserInstalled(..)
-
-      -- * Raw access to the Graph API
-    , getObject
-    , postObject
-    , deleteObject
-    , searchObjects
-    , Id(..)
-    , Argument
-
-      -- * Exceptions
-    , FacebookException(..)
-
-      -- * Internal functions
-    , unPermission
-    ) where
-
+-- * @FacebookT@ monad transformer
 import Facebook.Types
 import Facebook.Monad
 import Facebook.Base
