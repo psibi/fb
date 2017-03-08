ac6ad7f5963628fec4043b2c284a945649a77688
Sibi Prabakaran <sibi@psibi.in>
2017-03-08 11:52:59 +0530

    Add getPage_ function

--- src/Facebook/Object/Page.hs
+++ src/Facebook/Object/Page.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE DeriveDataTypeable
-           , FlexibleContexts
-           , OverloadedStrings
-           #-}
+{-# LANGUAGE DeriveDataTypeable, FlexibleContexts,
+  OverloadedStrings #-}
+
 module Facebook.Object.Page
-       ( Page(..)
-       , getPage
-       , searchPages
-       ) where
+  ( Page(..)
+  , getPage
+  , getPage_
+  , searchPages
+  ) where
 
 import Control.Applicative
 import Control.Monad (mzero)
@@ -27,52 +27,59 @@ import Facebook.Pager
 --
 -- /NOTE:/ Does not yet support all fields. Please file an issue if
 -- you need any other fields.
-data Page = Page { pageId                :: Id
-                 , pageName              :: Maybe Text
-                 , pageLink              :: Maybe Text
-                 , pageCategory          :: Maybe Text
-                 , pageIsPublished       :: Maybe Bool
-                 , pageCanPost           :: Maybe Bool
-                 , pageLikes             :: Maybe Integer
-                 , pageLocation          :: Maybe Location
-                 , pagePhone             :: Maybe Text
-                 , pageCheckins          :: Maybe Integer
-                 , pagePicture           :: Maybe Text
-                 , pageWebsite           :: Maybe Text
-                 , pageTalkingAboutCount :: Maybe Integer
-                 } deriving (Eq, Ord, Show, Read, Typeable)
+data Page = Page
+  { pageId :: Id
+  , pageName :: Maybe Text
+  , pageLink :: Maybe Text
+  , pageCategory :: Maybe Text
+  , pageIsPublished :: Maybe Bool
+  , pageCanPost :: Maybe Bool
+  , pageLikes :: Maybe Integer
+  , pageLocation :: Maybe Location
+  , pagePhone :: Maybe Text
+  , pageCheckins :: Maybe Integer
+  , pagePicture :: Maybe Text
+  , pageWebsite :: Maybe Text
+  , pageTalkingAboutCount :: Maybe Integer
+  } deriving (Eq, Ord, Show, Read, Typeable)
 
 instance A.FromJSON Page where
   parseJSON (A.Object v) =
-    Page <$> v .: "id"
-         <*> v .:? "name"
-         <*> v .:? "link"
-         <*> v .:? "category"
-         <*> v .:? "is_published"
-         <*> v .:? "can_post"
-         <*> v .:? "likes"
-         <*> v .:? "location"
-         <*> v .:? "phone"
-         <*> v .:? "checkin"
-         <*> v .:? "picture"
-         <*> v .:? "website"
-         <*> v .:? "talking_about_count"
+    Page <$> v .: "id" <*> v .:? "name" <*> v .:? "link" <*> v .:? "category" <*>
+    v .:? "is_published" <*>
+    v .:? "can_post" <*>
+    v .:? "likes" <*>
+    v .:? "location" <*>
+    v .:? "phone" <*>
+    v .:? "checkin" <*>
+    v .:? "picture" <*>
+    v .:? "website" <*>
+    v .:? "talking_about_count"
   parseJSON _ = mzero
 
+-- | Get a page using its ID. The user access token is optional.
+getPage
+  :: (R.MonadResource m, MonadBaseControl IO m)
+  => Id -- ^ Page ID
+  -> [Argument] -- ^ Arguments to be passed to Facebook
+  -> Maybe UserAccessToken -- ^ Optional user access token
+  -> FacebookT anyAuth m Page
+getPage id_ = getObject $ "/v2.8/" <> idCode id_
 
 -- | Get a page using its ID. The user access token is optional.
-getPage :: (R.MonadResource m, MonadBaseControl IO m)
-        => Id                    -- ^ Page ID
-        -> [Argument]            -- ^ Arguments to be passed to Facebook
-        -> Maybe UserAccessToken -- ^ Optional user access token
-        -> FacebookT anyAuth m Page
-getPage id_ = getObject $ "/" <> idCode id_
-
+getPage_
+  :: (R.MonadResource m, MonadBaseControl IO m)
+  => Id -- ^ Page ID
+  -> [Argument] -- ^ Arguments to be passed to Facebook
+  -> Maybe AppAccessToken -- ^ Optional user access token
+  -> FacebookT anyAuth m Page
+getPage_ id_ = getObject $ "/v2.8/" <> idCode id_
 
 -- | Search pages by keyword. The user access token is optional.
-searchPages :: (R.MonadResource m, MonadBaseControl IO m)
-            => Text                  -- ^ Keyword to search for
-            -> [Argument]            -- ^ Arguments to pass to Facebook
-            -> Maybe UserAccessToken -- ^ Optional user access token
-            -> FacebookT anyAuth m (Pager Page)
+searchPages
+  :: (R.MonadResource m, MonadBaseControl IO m)
+  => Text -- ^ Keyword to search for
+  -> [Argument] -- ^ Arguments to pass to Facebook
+  -> Maybe UserAccessToken -- ^ Optional user access token
+  -> FacebookT anyAuth m (Pager Page)
 searchPages = searchObjects "page"
